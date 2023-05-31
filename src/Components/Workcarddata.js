66b18bac991b643ed6df801c3dc12f01e52c1515
Mowumi <marwumi@gmail.com>
2023-05-31 12:01:41 +0100

    added a new project

--- src/Components/Workcarddata.js
+++ src/Components/Workcarddata.js
@@ -1,12 +1,13 @@
 import three from "../assets/aa.png"
 import five from "../assets/pulse.jpg"
 import six from "../assets/six.webp"
+import seven from "../assets/pp.png"
 
 const Projectcarddata = [
     {
         imgsrc:three,
         title: "E-commerce Website",
-        text: "Built with React and Redux",
+        text: "I developed a simple yet effective e-commerce website using React and Redux, offering a user-friendly interface and smooth online shopping experience.",
         view : "View",
         viewlink: "https://mowumi-foodcart.netlify.app/",
         source: "Source",
@@ -15,7 +16,7 @@ const Projectcarddata = [
     {
         imgsrc:five,
         title: "Pulse Website",
-        text: "Built with Html, Css and Js",
+        text: "I crafted a vibrant and engaging Pulse website using HTML, CSS, and JavaScript, delivering a visually appealing and interactive online presence.",
         view: "View",
         viewlink: "https://marwumi.github.io/pulse/",
         source: "Source",
@@ -24,11 +25,20 @@ const Projectcarddata = [
     {
         imgsrc:six,
         title: "Abstract landing page",
-        text: "Built with html and css.",
+        text: "I designed and developed an elegant and captivating abstract landing page using HTML and CSS, creating a visually stunning and immersive online experience",
         view: "View",
         viewlink: "https://marwumi.github.io/abstract/",
         source: "Source",
         sourcelink: "https://github.com/Marwumi/abstract"
+    },
+    {
+        imgsrc:seven,
+        title: "Recipe generator",
+        text: "I developed a React and Tailwind CSS-powered random recipe generator, utilizing Axios to fetch data from a meal API, resulting in a seamless and delightful user experience.",
+        view: "View",
+        viewlink: "https://mowumi-recipe.netlify.app/",
+        source: "Source",
+        sourcelink: "https://github.com/Marwumi/recipe-generator"
     }
 ]
 
